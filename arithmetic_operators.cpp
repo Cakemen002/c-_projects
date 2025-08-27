f83b690108313d92321e3c0c98b216cd01062a9c
Jeremy Angers <swat.angers@gmail.com>
2025-08-26 21:30:01 -0400

    arithmetic operatiors lesson

---
+++ arithmetic_operators.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+
+    int students = 23;
+    // students+=1;
+    // students++;
+
+    // students-=1;
+    // students--;
+
+    // students*=2;
+
+    // students/=2;
+
+    int remainer = students % 2;
+
+    std::cout << remainer << '\n';
+
+    return 0;
+}
