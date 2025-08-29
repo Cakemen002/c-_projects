5bdd72c41350c09baafccbee29fb5b40ad1d4b88
Jeremy Angers <swat.angers@gmail.com>
2025-08-28 21:39:31 -0400

    arrays lesson

---
+++ arrays.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int main(){
+
+    // std::string family[] = {"Jeremy", "Catherine", "Roxanne"};
+
+    std::string family[3];
+    family[0] = "Jeremy";
+    family[1] = "Catherine";
+    family[2] = "Roxanne";
+    
+    std::cout << family[0];
+
+    return 0;
+}
